b90f16ea125b65d6685794dcb4f164382f543f59
Hassan Shahid <127501753+hassans007@users.noreply.github.com>
2025-09-24 11:15:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Web-UI/", // e.g. "/portfolio/"
+  base: "/Web-UI/",
   server: {
     historyApiFallback: true,
   },
